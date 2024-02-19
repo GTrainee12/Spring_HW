03a3db829d211d1433bc41beefb5af9dc136838e
GTrainee12 <113129604+GTrainee12@users.noreply.github.com>
2024-02-19 18:33:51 +0300

    Урок 6. Проектирование и реализация API для серверного приложения.

--- Seminar6/example1sem6hw/src/main/java/org/example/example1sem6hw/model/Note.java
+++ Seminar6/example1sem6hw/src/main/java/org/example/example1sem6hw/model/Note.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table(name = "Note")
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- Seminar6/example1sem6hw/src/main/java/org/example/example1sem6hw/repository/NoteRepository.java
+++ Seminar6/example1sem6hw/src/main/java/org/example/example1sem6hw/repository/NoteRepository.java
@@ -2,6 +2,9 @@ package org.example.example1sem6hw.repository;
 
 import org.example.example1sem6hw.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 }
